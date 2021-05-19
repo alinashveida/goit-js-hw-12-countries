8de2aee27709c47b79b9bc523ebffb090df55530
alinashveida <alinashveida@gmail.com>
2021-05-19 17:11:52 +0300

    4

--- src/index.js
+++ src/index.js
@@ -4,40 +4,43 @@ import countriesList from './templates/countriesList.hbs'
 import API from './js/fetchCountries';
 import getRefs from './js/get-refs';
 
+import { info, notice, alert, error} from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+
 const debounce = require('lodash.debounce');
-console.log(debounce);
 
  const refs = getRefs();
 refs.formInput.addEventListener('input', debounce(() => {
     onInput();
   }, 500));
 
-function onInput(evt){
-    // evt.preventDefault()
-    
+function onInput(evt){З
     const inputValue = refs.formInput.value;
     console.log(inputValue)
 
-    
     API.fetchCountries(inputValue)
    .then(checkNumberOfCountries)
    .catch(onFetchError)
-//    .finally(() => inputValue.reset())
 }
 
-
-function onFetchError(error){
+function onFetchError(){
+    error({
+        title: "Ошибка",
+        text: "Страна не найдена",
+        autoOpen: true,
+        minHeight: '16px',
+        maxTextHeight: null,
+        animateSpeed: 'normal',
+        shadow: true,
+        delay: 1500
+    })
     
-    alert('Страна ненайдена')
-
 }
-
-//------------------------------///////////////////////
 
 function renderCountryCard(country){
     console.log(country)
     const markUp = cardTemplate(country[0]);
-    console.log(markUp);
     refs.card.innerHTML = markUp;
 
 }
@@ -45,7 +48,6 @@ function renderCountryCard(country){
 function renderCounthiesList(country){
      const markUpList = countriesList(country);
      refs.card.innerHTML = markUpList;
-     console.log(markUpList)
 }
 
 function checkNumberOfCountries (countries){
@@ -53,21 +55,28 @@ function checkNumberOfCountries (countries){
         clearMarkUp();
         renderCounthiesList(countries);
         console.log(countries);
-        console.log('1')
     }
      else if (countries.length === 1){
         clearMarkUp();
         renderCountryCard(countries);
-        console.log('2')
     }
      else if(countries.length > 10){
         clearMarkUp();
-         console.log('too')
+        alert({
+            title: "Ошибка",
+            text: "Слишком много совпадений",
+            autoOpen: true,
+            minHeight: '16px',
+            maxTextHeight: null,
+            animateSpeed: 'normal',
+            shadow: true,
+            delay: 1500
+        })
 
      }
      else{
         clearMarkUp();
-        console.log('else')
+        onFetchError();
     }
 
     
@@ -78,78 +87,6 @@ function clearMarkUp(){
 }
 
 
-
-//---------------------------------------------------------------------
-// const BASE_URL = 'https://restcountries.eu/rest/v2/name';
-
-// const fetchCountry = country => fetch(`${BASE_URL}/${country}`).then(response => response.json());
-
-
-
-// function fetchCountry(country){
-//     fetch(`${BASE_URL}/${country}`).then((response) =>{
-//     return response.json()
-// })
-// }
-
-// fetchCountry(u).then(renderCountryCard).catch((error) => console.log(error))
-//--------------------------------------------
-
-
-
-// function fetchCountry(name){
-//     name = "usa"
-//     fetch(`https://restcountries.eu/rest/v2/name/${name}`).then((response) =>{
-//     return response.json()
-// })
-// .then(renderCountryCard)
-// .catch((error) =>{
-//     console.log(error)
-// });
-// }
-// fetchCountry();
-
-
-
-
-
-//------------------------------notificashca
-// import { info, notice } from "@pnotify/core";
-// import "@pnotify/core/dist/PNotify.css";
-// import "@pnotify/core/dist/BrightTheme.css";
-// import * as Confirm from "@pnotify/confirm";
-// import "@pnotify/confirm/dist/PNotifyConfirm.css";
-// console.log(notice)
-
-
-// function click() {
-//     info({
-//       title: "Button Clicked",
-//       text:
-//         "You have clicked the button. You may now complete the process of reading the notice.",
-//       modules: new Map([
-//         [
-//           Confirm,
-//           {
-//             confirm: true,
-//             buttons: [
-//               {
-//                 text: "Ok",
-//                 primary: true,
-//                 click: notice => {
-//                   notice.close();
-//                 }
-//               }
-//             ]
-//           }
-//         ]
-//       ])
-//     });
-//   }
-
-//   const a = document.querySelector('.form-btn');
-
-//   a.addEventListener('click', click)
 
  
 
--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -2,6 +2,5 @@ export default function  getRefs() {
     return {
         card: document.querySelector(".card"),
         formInput: document.querySelector(".form-input"),
-    // formBtn: document.querySelector(".form-btn")
 }
 }
